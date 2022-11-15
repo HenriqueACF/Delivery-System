580e469ef67ea8ad5ce2e4af90a4830050f81353
HenriqueACF <henrique.assis145@gmail.com>
2022-11-15 13:51:13 -0300

    feat: [forget passwrod] -> tela de email enviado

--- saas_delivery/pages/[tenant]/forget-success.tsx
+++ saas_delivery/pages/[tenant]/forget-success.tsx
@@ -1,65 +1,54 @@
-import styles from '../../styles/Forget.module.css'
+import styles from '../../styles/ForgetSuccess.module.css'
 import { UseApi } from "../../libs/useApi";
 import { GetServerSideProps } from "next";
 import { Tenant } from "../../types/Tenant";
 import { useAppContext } from "../../contexts/AppContext";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Head from "next/head";
 import { Header } from '../../components/Header';
-import { InputField } from '../../components/InputField';
 import { Button } from '../../components/Button';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { Icon } from '../../components/Icon';
 
-const Forget = (data: Props) =>{
+const ForgetSuccess = (data: Props) =>{
 
     const router = useRouter()
     const {tenant, setTenant} = useAppContext()
-
-    const [email, setEmail] = useState('')
 
     useEffect(()=>{
         setTenant(data.tenant)
     }, [])
 
     const handleSubmit = () => {
-        router.push(`/${data.tenant.slug}/forget-success`)
+        router.push(`/${data.tenant.slug}/login`)
     }
 
     return (
         <div className={styles.container}>
             <Head>
-                <title>Forget My Password | {data.tenant.name}</title>
+                <title>Forget My  | {data.tenant.name}</title>
             </Head>
 
-            <Header color={data.tenant.mainColor} backHref={`/${data.tenant.slug}/login`}/>
+            <Header color={data.tenant.mainColor} backHref={`/${data.tenant.slug}/forget`}/>
 
-            <div className={styles.header}>{data.tenant.name}</div>
+            <div className={styles.iconArea}>
+                <Icon 
+                    icon='mailSent' 
+                    color={data.tenant.mainColor}
+                    width={99}
+                    height={81}
+                />
+            </div>
 
-            <div className={styles.title}>Esqueceu sua senha?</div>
+            <div className={styles.title}>Verifique seu e-mail</div>
 
-            <div 
-                className={styles.subtitle}
-                style={{borderBottomColor: data.tenant.mainColor}}
-            >Preencha o campo com seu e-mail e receba as instruções necessárias para redefinir a sua senha.</div>
-
-            <div className={styles.line}></div>
-
+            <div className={styles.subtitle}>
+                Enviamos as instruções para recuperação de senha para o seu e-mail.</div>
             <div className={styles.formArea}>
-
-                <div className={styles.inputArea}>
-                    <InputField
-                        color={data.tenant.mainColor}
-                        placeholder="Digite seu e-mail"
-                        value={email}
-                        onChange={setEmail}
-                    />
-                </div>
-
                 <div className={styles.inputArea}>
                     <Button
                         color={data.tenant.mainColor}
-                        label="Enviar"
+                        label="Fazer Login"
                         onClick={handleSubmit}
                         fill
                     />
@@ -71,7 +60,7 @@ const Forget = (data: Props) =>{
     )
 }
 
-export default Forget
+export default ForgetSuccess
 
 type Props = {
     tenant: Tenant
