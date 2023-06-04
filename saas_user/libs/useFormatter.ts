07d15c76b07df40335cb9892170d5b506434eaf6
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-06-04 17:50:02 -0300

    feat(useFormater): create a util to format date

--- saas_user/libs/useFormatter.ts
+++ saas_user/libs/useFormatter.ts
@@ -11,5 +11,8 @@ export const useFormatter = () => ({
 
         const remain = minDigits - qt.toString().length
         return `${'0'.repeat(remain)}${qt}`
+    },
+    formatDate:(date: string)=>{
+        return new Intl.DateTimeFormat('pt-BR').format(new Date(`${date} 00:00:00`))
     }
 })
