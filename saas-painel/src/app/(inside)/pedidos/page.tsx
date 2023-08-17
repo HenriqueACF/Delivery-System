5bcb5e147d16c792586d237b5cd0eeb83b4fc0f4
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-08-17 14:50:30 -0300

    style: add skeleton

--- saas-painel/src/app/(inside)/pedidos/page.tsx
+++ saas-painel/src/app/(inside)/pedidos/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import {useState} from "react";
-import {Box, Button, InputAdornment, TextField, Typography} from "@mui/material";
+import {Box, Button, CircularProgress, Grid, InputAdornment, Skeleton, TextField, Typography} from "@mui/material";
 import {Refresh, Search} from "@mui/icons-material";
 
 const Page = () => {
     const [searchInput, setSearchInput] = useState('')
+    const [loading, setLoading] = useState(false)
 
     const handleSearchInput = () => {
 
@@ -17,23 +18,26 @@ const Page = () => {
 
     return (
         <Box sx={{my: 3}}>
-            <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
+            <Box sx={{mb: 3, display: 'flex', justifyContent: 'space-between'}}>
                 <Box sx={{display: 'flex', alignItems: 'center'}}>
                     <Typography
                         component="h5"
                         variant="h5"
                         sx={{color: '#555', mr: 2}}>Pedidos
                     </Typography>
-                    <Button sx={{justifyContent: {xs: 'flex-start', md: 'center'}}}>
-                        <Refresh/>
-                        <Typography
-                            component="div"
-                            sx={{color: '#555', display: {xs: 'none', sm: 'block'}}
-                            }
-                        >
-                            Atualizar
-                        </Typography>
-                    </Button>
+                    {loading && <CircularProgress size={24}/>}
+                    {!loading &&
+                        <Button sx={{justifyContent: {xs: 'flex-start', md: 'center'}}}>
+                            <Refresh/>
+                            <Typography
+                                component="div"
+                                sx={{color: '#555', display: {xs: 'none', sm: 'block'}}
+                                }
+                            >
+                                Atualizar
+                            </Typography>
+                        </Button>
+                    }
                 </Box>
                 <TextField
                     value={searchInput}
@@ -41,6 +45,7 @@ const Page = () => {
                     onKeyUp={handleSearchKey}
                     placeholder="Pesquise um pedido"
                     variant="standard"
+                    disabled={loading}
                     InputProps={{
                         endAdornment: (
                             <InputAdornment position="end">
@@ -52,6 +57,28 @@ const Page = () => {
 
                 </TextField>
             </Box>
+            <Grid
+                container
+                spacing={3}
+                columns={{xs: 1, sm: 2, md: 4}}
+            >
+                {loading &&
+                    <>
+                        <Grid item xs={1}>
+                            <Skeleton variant="rectangular" height={220}/>
+                        </Grid>
+                        <Grid item xs={1}>
+                            <Skeleton variant="rectangular" height={220}/>
+                        </Grid>
+                        <Grid item xs={1}>
+                            <Skeleton variant="rectangular" height={220}/>
+                        </Grid>
+                        <Grid item xs={1}>
+                            <Skeleton variant="rectangular" height={220}/>
+                        </Grid>
+                    </>
+                }
+            </Grid>
         </Box>
     )
 }
