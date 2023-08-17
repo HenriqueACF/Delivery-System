05c4a0346a5fd689191d43b5a9aaf56c7179252f
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-08-17 10:04:02 -0300

    feat: inicio do layout das telas internas

---
+++ saas-painel/src/app/(inside)/pedidos/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import {Box} from "@mui/material";
+import {Header} from "@/components/Header";
+
+const Page = () => {
+    return (
+        <Box>
+            Página de pedigos
+        </Box>
+    )
+}
+
+export default Page
